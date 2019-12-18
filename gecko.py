4baf24faac4ca31a9aaf7e97df053f38b94f9708
leotada <leotada523@gmail.com>
2019-12-18 20:48:38 -0300

    Async version

--- gecko.py
+++ gecko.py
@@ -1,9 +1,10 @@
-from urllib import request
-import urllib
 from http import HTTPStatus
 from typing import List, Optional, Dict
 import re
 import json
+import asyncio
+
+import httpx
 
 
 def get_domain(url: str) -> Optional[str]:
@@ -12,20 +13,6 @@ def get_domain(url: str) -> Optional[str]:
     """
     match = re.search(r"([\w-]+\.)+(\w+)", url)
     return match.group(0) if match is not None else None
-
-
-def get_html_from_url(url: str, timeout: int = 10) -> Optional[str]:
-    """
-    Get HTML string after making a url request
-    """
-    try:
-        req = request.urlopen(url, timeout=timeout)
-        if req.status == HTTPStatus.OK:
-            data: str = req.read().decode('utf-8')
-            return data
-        return None
-    except urllib.error.HTTPError:
-        return None
 
 
 def find_links(html: str) -> List[str]:
@@ -57,7 +44,21 @@ def find_assets(html: str) -> List[str]:
         html, flags=re.IGNORECASE)
 
 
-def search_on_page(root_url: str, base_url: str, result_map: dict) -> bool:
+async def get_html_from_url(url: str, timeout: int = 10) -> Optional[str]:
+    """
+    Get HTML string after making a url request
+    """
+    try:
+        req = await httpx.get(url, timeout=timeout)
+        if req.status_code == HTTPStatus.OK:
+            data: str = req.text
+            return data
+        return None
+    except httpx.HTTPError:
+        return None
+
+
+async def search_on_page(root_url: str, base_url: str, result_map: dict) -> bool:
     """
     Receive a url and a result map and do a search of links and assets on the
     page accessing url.
@@ -67,7 +68,7 @@ def search_on_page(root_url: str, base_url: str, result_map: dict) -> bool:
         return False
 
     result: Dict = {'urls': [], 'assets': []}
-    page_html = get_html_from_url(base_url)
+    page_html = await get_html_from_url(base_url)
     if page_html is None:
         return False
 
@@ -88,28 +89,39 @@ def search_on_page(root_url: str, base_url: str, result_map: dict) -> bool:
     return True
 
 
-def main() -> None:
-    root_url = 'https://elixir-lang.org'
+async def run_task(root_url: str, current_url: str,
+                   result_map: dict) -> Optional[List[str]]:
+    print('URL: ', current_url)
+    result = await search_on_page(root_url, current_url, result_map)
+    if result:
+        return result_map[current_url]['urls']
+    return None
+
+
+async def main() -> None:
+    root_url = 'https://duckduckgo.com'
     save_urls = False
     result_map: Dict[str, Dict] = {}
     queue = [root_url]
     count = 0
-
-    def run() -> int:
-        count = 0
-        current_url = queue[0]
-        print('URL: ', current_url)
-        result = search_on_page(root_url, current_url, result_map)
-        if result:
-            count += 1
-            queue.extend(result_map[current_url]['urls'])
-        queue.pop(0)
-        return count
+    concurrency = 5
 
     while queue:
-        r = run()
-        count += r
-        print('Queue: ', len(queue))
+        if len(queue) > concurrency:
+            tasks = []
+            for _ in range(concurrency):
+                tasks.append(run_task(root_url, queue.pop(0), result_map))
+            for response in await asyncio.gather(*tasks):
+                if response:
+                    queue.extend(response)
+                    count += 1
+        else:
+            current_url = queue.pop(0)
+            response = await run_task(root_url, current_url, result_map)
+            if response:
+                queue.extend(response)
+                count += 1
+        print('Queue:', len(queue))
         print(f'Requests: {count}')
 
     if not save_urls:
@@ -123,4 +135,4 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
